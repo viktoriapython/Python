216bf580a71cc86d6b1cc2d2772c068071cec8cc
viktoriapython <78961157+viktoriapython@users.noreply.github.com>
2021-02-16 12:50:01 +0200

    Test practice

---
+++ Test_viktoria/program2.py
@@ -0,0 +1,11 @@
+from math import sqrt
+a = 3
+b = 4
+print('Input length of shorter triangle sides:', a, b )
+
+c = ('The length of the hypotenuse')
+
+
+c = sqrt(a**2 + b**2)
+
+print('The length of the hypotenuse is', c)
